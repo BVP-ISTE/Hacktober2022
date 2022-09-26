903fa9813dff557ae34ae2c64868dd1199ab125a
Taanvee <taanvee.sharma@gmail.com>
2022-09-26 23:57:56 +0530

    Printing tokens file added

--- C/Task/IndicesToSum.c
+++ C/Task/IndicesToSum.c
@@ -1,6 +1,6 @@
 // We are given an array of integers nums and an integer target, return indices of the two numbers such that they add upto target.
 
-//You may assume that each input would have exactky one solution and you may not use the same element twice.
+//You may assume that each input would have exactly one solution and you may not use the same element twice.
 
 //Input:
 //First line consists of array of integers
