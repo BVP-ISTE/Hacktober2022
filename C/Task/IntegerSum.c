443843511981528e71db90861a72798a7067ab05
Taanvee <taanvee.sharma@gmail.com>
2022-09-28 20:01:00 +0530

    IntegerSum added

---
+++ C/Task/IntegerSum.c
@@ -0,0 +1,13 @@
+//Given a positive integer N. How many ordered pairs of positive integers (i, j) exist such that i + j = N.
+
+//Note that since the pairs are ordered, (1, 2) and (2,1) are considered different.
+
+//Input Format
+//The first and only line of input contains a single integer N.
+
+//Output Format
+//Print a single integer, the number of ordered pairs of positive integers (i,j) such that i + j = N.
+
+//Sample:
+//Input: 1
+//Output: 2
