4c48357258807c750af0f3c1ecbc0b253331b39d
Reeba Qureshi <64488642+reeba212@users.noreply.github.com>
2022-10-01 23:35:36 +0530

    Add files via upload

---
+++ python/error/leaders_in_array.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#An array element A[i] is called Leader, if all the elements following it (i.e. present at its right) are less than or equal to A[i].
+#find the error in the function and correct it
+def printLeaders(arr,n): 
+    for i in range(0,n):
+        l=True
+        for j in range(i+1,n):
+            if arr[j]<arr[i]:
+                l=False
+                break
+        if l:
+            print(arr[i],end=" ")
+
+n=int(input())
+arr=[int(x) for x in input().strip().split()]
+printLeaders(arr,n)
+
