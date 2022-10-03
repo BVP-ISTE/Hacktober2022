8054c57bb330ed12160563af262439de3d7f4fe5
Taanvee <taanvee.sharma@gmail.com>
2022-10-03 23:46:15 +0530

    LCM added

---
+++ C/Task/LCM.c
@@ -0,0 +1,14 @@
+//Write a program in C to find LCM of any two numbers.
+
+//Input format:
+ //Two integers a and b
+
+ //Output format:
+ //LCM of a and b
+
+ //Sample:
+ //Input:
+ //15 20
+
+ //Output:
+ //60
