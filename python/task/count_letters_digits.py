1578053195e1a247b145998de754bd796d05f3e6
Reeba Qureshi <64488642+reeba212@users.noreply.github.com>
2022-10-03 22:11:14 +0530

    Add files via upload

---
+++ python/task/count_letters_digits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+"""
+Write a program that accepts a sentence and calculate the number of letters and digits.
+Suppose the following input is supplied to the program:
+hello world! 123
+Then, the output should be:
+LETTERS 10
+DIGITS 3
+"""
+
---
+++ python/task/greatest_number.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+"""
+Write a program to find the greatest number out of three numbers
+"""
+
