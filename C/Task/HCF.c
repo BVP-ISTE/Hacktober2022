f637f28a4d4e9485dbed5224f4bb0f9661456b4f
Taanvee <taanvee.sharma@gmail.com>
2022-10-03 23:44:04 +0530

    HCF added

---
+++ C/Task/HCF.c
@@ -0,0 +1,14 @@
+ //Write a C program to find HCF (Highest Common Factor) of two numbers. 
+
+ //Input format:
+ //Two integers a and b
+
+ //Output format:
+ //HCF of a and b
+
+ //Sample:
+ //Input:
+ //24 28
+
+ //Output:
+ //4
