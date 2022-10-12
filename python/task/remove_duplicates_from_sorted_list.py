aa9273a4e97b992119a152b941dc4f981225de33
Navin <patwarinavin9@gmail.com>
2022-10-13 01:20:16 +0530

    Solved the task to remove duplicates from list in python

--- python/task/remove_duplicates_from_sorted_list.py
+++ python/task/remove_duplicates_from_sorted_list.py
@@ -7,3 +7,23 @@
 # Example 2:
 # Input: head = [1,1,2,3,3]
 # Output: [1,2,3]
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head == None:
+            return head
+        t = head
+        ans = head
+        a = -101
+        while(t.next != None):
+            if t.next.val != t.val or t.val == a:
+                head.next = t.next
+                head = head.next
+                a = t.val
+            t = t.next
+        head.next = None
+        return ans
