b82e6a658e886a9ee10815504cf73d62b9e6409d
Bhavuk Khanna <56162838+bhavuk-09@users.noreply.github.com>
2022-10-13 21:09:16 +0530

    Update countpallindromicsubstrings.cpp
    Solved Issue #138

--- CPP/error/countpallindromicsubstrings.cpp
+++ CPP/error/countpallindromicsubstrings.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 bool isPalindrome(string str){
     for (int i = 0; i<=str.length(); i++) {
-        if(str[i]==str[str.length()-i-1]){
+        if(str[i]!=str[str.length()-i-1]){
             return false;
         }
 
