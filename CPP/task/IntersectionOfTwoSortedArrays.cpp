53b9497d496a0f6fe5ea577f8769852d74610749
tanmaysharma17 <73175318+tanmaysharma17@users.noreply.github.com>
2022-10-14 04:56:40 +0530

    Update IntersectionOfTwoSortedArrays.cpp
    Done, pls merge the pr @ela2003 @abhinavcodedeveloper @divya520357

--- CPP/task/IntersectionOfTwoSortedArrays.cpp
+++ CPP/task/IntersectionOfTwoSortedArrays.cpp
@@ -19,3 +19,45 @@
 //Output Format:
 // The only line of output of each test case contains 'K' space-separated integers which correspond to the intersection of the two arrays A and B.
 // The output of each test case will be printed on a separate line.
+
+/*
+    Time Complexity: O(N+M)
+    Space Complexity: O(N)
+
+    Where 'N' is the size of the first array and 
+    'M' is the size of the second array.
+*/
+
+#include <unordered_map>
+
+vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, int m)
+{
+    // Declare an array to store answer.
+    vector<int> ans;
+
+    unordered_map<int, int> mp;
+
+    // Hashing the elements of the first array
+    for (int i = 0; i < n; i++)
+    {
+        mp[arr1[i]]++;
+    }
+
+    for (int j = 0; j < m; j++)
+    {
+        // Checking if the elements are present in the second array or not.
+        if (mp.count(arr2[j]) != 0)
+        {
+            ans.push_back(arr2[j]);
+            mp[arr2[j]]--;
+
+            // Deleting the element if it's frequency is 0.
+            if (mp[arr2[j]] == 0)
+            {
+                mp.erase(arr2[j]);
+            }
+        }
+    }
+
+    return ans;
+}
