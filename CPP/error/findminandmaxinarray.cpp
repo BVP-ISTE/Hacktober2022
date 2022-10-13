0a9914dd0455171ed0488bd2a302b2dae81cd395
Bhavuk Khanna <56162838+bhavuk-09@users.noreply.github.com>
2022-10-13 20:05:20 +0530

    Update findminandmaxinarray.cpp
    Solved issue #36

--- CPP/error/findminandmaxinarray.cpp
+++ CPP/error/findminandmaxinarray.cpp
@@ -15,7 +15,7 @@ int minNo = INT_MAX;
 for (int i = 0; i < n; i++)
 {
     maxNo = max(maxNo,arr[i]);
-     minNo = max(minNo,arr[i]);
+    minNo = min(minNo,arr[i]);
 }
 cout<<maxNo<<" "<<minNo<<endl;
 return 0;
