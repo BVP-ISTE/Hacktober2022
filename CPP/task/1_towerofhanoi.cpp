89b2ff80c4dbc364bccf09af64dda13838eb60f1
beingfarazkhan <faraz.khan14158124@gmail.com>
2022-10-14 00:09:40 +0530

    solved HCF , LCM and Tower of hanoi issue

--- CPP/task/1_towerofhanoi.cpp
+++ CPP/task/1_towerofhanoi.cpp
@@ -1,1 +1,29 @@
-//write a recursive function of tower of hanoi
+// write a recursive function of tower of hanoi
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+void tower_of_hanoi(int, char, char, char);
+
+int main()
+{
+    int n;
+
+    printf("Enter the number of discs : ");
+    scanf("%d", &n);
+    tower_of_hanoi(n, 'A', 'B', 'C');
+    return 0;
+}
+void tower_of_hanoi(int n, char from_rod, char aux_rod, char to_rod)
+{
+    if (n == 1)
+    {
+        printf("Move Disc 1 From %c to %c \n", from_rod, to_rod);
+        return;
+    }
+
+    tower_of_hanoi(n - 1, from_rod, to_rod, aux_rod);
+    printf("Move Disc %d From %c to %c \n", n, from_rod, to_rod);
+    tower_of_hanoi(n - 1, aux_rod, from_rod, to_rod);
+}
