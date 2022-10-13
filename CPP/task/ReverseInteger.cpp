05a1f37285c9441495cce08a31bb859fb8f9d052
Bhavuk Khanna <56162838+bhavuk-09@users.noreply.github.com>
2022-10-13 19:38:23 +0530

    Update ReverseInteger.cpp
    Issue Number #55 Solved

--- CPP/task/ReverseInteger.cpp
+++ CPP/task/ReverseInteger.cpp
@@ -1,6 +1,26 @@
 //Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
 
 // Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+#include <iostream>
+using namespace std;
+int reverseInteger(int x){
+    if(x<-231 or x>231){
+        return 0;
+    }
+    int rev=0;
+    while(x!=0){
+        rev= rev*10+ x%10;
+        x=x/10;
+    }
+    return rev;
+}
+
+int main() {
+    int x;
+    cin>>x;
+    int res=reverseInteger(x);
+    cout<<res;
+}
 
  
 
