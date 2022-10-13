ad35b3598b15f28521ad46ac255923a90ce850d0
Faiz Khan <72645960+FaizFk@users.noreply.github.com>
2022-10-13 19:42:07 +0530

    Added Solution to SumofMultiples

--- C/Task/SumofMultiples.c
+++ C/Task/SumofMultiples.c
@@ -1,4 +1,19 @@
 //Write a program in C to find the number and sum of all integer between 100 and 200 which are divisible by 9.
 
+#include <stdio.h>
+
+int main()
+{
+    int sum=0;
+    for(int i=100;i<=200;i++)
+    {
+        if(i%9==0)
+        {
+            sum+=i;
+        }
+    }
+    printf("%d",sum);
+}
+
 //Output Expected:
 //1683
