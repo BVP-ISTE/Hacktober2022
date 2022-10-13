b17d7cd740f0d6d44995395223ef272e6b7ee976
triptiii3 <111265499+triptiii3@users.noreply.github.com>
2022-10-13 12:41:01 +0530

    twochanges

--- CPP/task/Binary_to_decimal.cpp
+++ CPP/task/Binary_to_decimal.cpp
@@ -1,1 +1,30 @@
 // Take N (number in binary format). Write a function that converts it to decimal format and Print the value returned.
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+// function prototype
+int convert(long long);
+
+int main() {
+  long long n;
+  
+  cin >> n;
+  cout <<convert(n) ;
+  return 0;
+}
+
+// function definition
+int convert(long long n) {
+  int dec = 0, i = 0, rem;
+
+  while (n!=0) {
+    rem = n % 10;
+    n /= 10;
+    dec += rem * pow(2, i);
+    ++i;
+  }
+
+  return dec;
+}
