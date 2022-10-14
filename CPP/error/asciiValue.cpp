16bc1fb741a97c2e736cb199ed82f9e21901695c
Muskan Rathore <62328534+Sakura74@users.noreply.github.com>
2022-10-14 22:55:14 +0530

    Update asciiValue.cpp
    Solved #17

--- CPP/error/asciiValue.cpp
+++ CPP/error/asciiValue.cpp
@@ -1,11 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-    char c,i;
-for (int i = 0; i < 255; i++)
+    int c;
+for (int i = 0; i<= 255; i++)
 {
-    c = 0;
-    cout<<"ascii value: "<<i<<"  ascii char: "<<c<<endl;
+    
+    cout<<"ascii value: "<<i<<"  ascii char: "<<(char)i<<endl;
+    
   
 }
+
+return 0;
+
 }
