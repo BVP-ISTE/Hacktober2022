6ef56af21b4e5a734ee6651cc6d764d0b1b4ba4e
akash-1107 <110085080+akash-1107@users.noreply.github.com>
2022-10-15 16:24:54 +0530

    Update Shopping_game.cpp

--- CPP/task/Shopping_game.cpp
+++ CPP/task/Shopping_game.cpp
@@ -25,3 +25,37 @@
 // Aayush shops for 1 smartphone first, and then Harshit shops for 2 smartphones, then Aayush shops for 3 smartphones. 
 // Now Harshit shops for 4 smartphones (he has 2 + 4 = 6 smartphones in total, which is allowed because it doesn't exceed N). Then Aayush tries to shop for 5 smartphones but he can't because 1 + 3 + 5 = 9 which is greater than M. 
 // Aayush loses and Harshit is the winner.
+
+
+
+//solution
+#include <iostream>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int aayush_max, harshit_max;
+        cin>>aayush_max>>harshit_max;
+
+		int aayush_count = 0;
+		int harshit_count = 0;
+		for (int i = 1;; i++) {
+			if (i % 2 == 0) {
+				if (harshit_count + i <= harshit_max) {
+					harshit_count = harshit_count + i;
+				} else {
+					cout<<"Aayush"<<endl;
+					break;
+				}
+			} else {
+				if (aayush_count + i <= aayush_max) {
+					aayush_count = aayush_count + i;
+				} else {
+					cout<<"Harshit"<<endl;
+					break;
+				}
+			}
+		}
+    }
+}
