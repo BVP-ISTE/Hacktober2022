3fffd9796813540bfa28372ec03c3d39f2fdfa73
Parth Barara <parthbarara10@gmail.com>
2022-10-16 00:14:05 +0530

    Solved the problem #132

--- python/task/sqrt_x.py
+++ python/task/sqrt_x.py
@@ -10,3 +10,32 @@
 # Input: x = 8
 # Output: 2
 # Explanation: The square root of 8 is 2.82842..., and since the decimal part is truncated, 2 is returned.
+
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        
+        if x==0 or x==1:
+            return x
+        
+        start = 1
+        end = x
+        
+        while start <= end:
+            mid = (start + end)//2
+            
+            mid_sq = mid*mid
+            
+            if mid_sq == x:
+                return mid
+            
+            if mid_sq < x:
+                start = mid+1
+                ans = mid
+            else : 
+                    end = mid-1
+                    
+                    return ans
