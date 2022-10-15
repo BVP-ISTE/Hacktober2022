fe108e807d83ad6ada17805104b8276389e7e954
Swayam Gupta <77153230+swayamg20@users.noreply.github.com>
2022-10-15 12:30:51 +0530

    Updated Palindrome.c for #147

--- C/Task/Palindrome.c
+++ C/Task/Palindrome.c
@@ -4,3 +4,19 @@
 //121
 //Output:
 //121 is a palindrome number.
+#include <stdio.h>
+int main() {
+  int n, reversed = 0, remainder, original;
+    scanf("%d", &n);
+    original = n;
+    while (n != 0) {
+        remainder = n % 10;
+        reversed = reversed * 10 + remainder;
+        n /= 10;
+    }
+    if (original == reversed)
+        printf("%d is a palindrome.", original);
+    else
+        printf("%d is not a palindrome.", original);
+
+    return 0;
