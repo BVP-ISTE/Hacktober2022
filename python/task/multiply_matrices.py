ea9afdd458d42db7d9a6caee2c35c2c9742701c9
Aditi Chauhan <aditichauhan50@gmail.com>
2022-10-15 21:51:28 +0545

    Added code of multiplying matrices through python

--- python/task/multiply_matrices.py
+++ python/task/multiply_matrices.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+
 # coding: utf-8
 
 # In[ ]:
@@ -15,3 +15,32 @@ Y = [ [5,8,1,2],
           [6,7,3,0],
           [4,5,9,1] ]
 
+
+# we can change matrices by altering the values of the matrices.
+
+# 1st Matrix
+X = [[12, 7, 3],
+	[4, 5, 6],
+	[7, 8, 9]]
+
+# 2nd Matrix
+Y = [[5, 8, 1, 2],
+	[6, 7, 3, 0],
+	[4, 5, 9, 1]]
+	
+result= [[0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]]
+
+#Because when we multiply 3X3 (AXB) matrix to a 3X4 (PXQ) matrix, the resultant matrix is of the order 3X4 (AXQ)
+
+
+# Code to multiply them 
+
+for i in range(len(X)):
+	for j in range(len(Y[0])):
+		for k in range(len(Y)):
+			result[i][j] += X[i][k] * Y[k][j]
+
+for res in result:
+	print(res)
