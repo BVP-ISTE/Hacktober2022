fc74a04148033dcc1fe5eb13b0c1def81d953fed
Ankit Kumar <111765525+Ankit-0009@users.noreply.github.com>
2022-10-16 01:09:16 +0530

    Add files via upload

---
+++ CPP/task/Decimaltooctal.cpp
@@ -0,0 +1,15 @@
+// Take N (number in decimal format). Write a function that converts it to octal format. Print the value returned.
+
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int n,f=0,c=0;
+	cin>>n;
+	while(n!=0){
+		f=f+((n%8)*pow(10,c));
+		c++;
+		n=n/8;
+	}
+	cout<<f;
+	return 0;
+}
