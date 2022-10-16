ba748bf56d111836f5f8739142769aa0efc70af4
akash-1107 <110085080+akash-1107@users.noreply.github.com>
2022-10-16 10:26:56 +0530

    Update Van_nueman_loves_binary.cpp

--- CPP/task/Van_nueman_loves_binary.cpp
+++ CPP/task/Van_nueman_loves_binary.cpp
@@ -13,4 +13,29 @@
 15
 6
 63*/
+//solution
+#include <iostream>
+using namespace std;
 
+int main()
+{
+int n;
+cin>>n;
+
+    for(int i = 1; i<=n; i++)
+   {
+          int num;
+          cin>>num;
+          int dvalue = 0;
+          int base = 1;
+
+          while (num){
+                  int last_digit = num % 10;
+                  num = num / 10;
+                  dvalue += last_digit * base;
+                  base = base * 2;
+          }
+         cout<<dvalue<<endl;
+  }
+  return 0;
+}
