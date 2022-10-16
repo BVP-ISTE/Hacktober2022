3e88f85ecc0c2f8ca065b120dc01733d14c9bcd0
Swayam Gupta <77153230+swayamg20@users.noreply.github.com>
2022-10-17 04:57:13 +0530

    Updated 1_ugly_number.cpp for #68

--- CPP/task/1_ugly_number.cpp
+++ CPP/task/1_ugly_number.cpp
@@ -5,3 +5,45 @@
 // Input: n = 10
 // Output: 12
 // Explanation: [1, 2, 3, 4, 5, 6, 8, 9, 10, 12] is the sequence of the first 10 ugly numbers.
+#include <iostream>
+using namespace std;
+
+int maxDivide(int a, int b)
+{
+	while (a % b == 0)
+		a = a / b;
+		
+	return a;
+}
+
+int isUgly(int no)
+{
+	no = maxDivide(no, 2);
+	no = maxDivide(no, 3);
+	no = maxDivide(no, 5);
+
+	return (no == 1) ? 1 : 0;
+}
+
+
+int getNthUglyNo(int n)
+{
+	int i = 1;
+	int count = 1;
+	while (n > count)
+	{
+		i++;
+		if (isUgly(i))
+			count++;
+	}
+	return i;
+}
+
+int main()
+{
+   int n;
+   cin>>n;
+	unsigned no = getNthUglyNo(n);
+	cout << no;
+	return 0;
+}
