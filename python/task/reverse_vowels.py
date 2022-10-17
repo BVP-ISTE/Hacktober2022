5d88d3152d6dc8b3fe35997ca7e79ea94dc4506a
Vaibhav Sangwan <94783049+vaibhav-sangwan@users.noreply.github.com>
2022-10-17 18:55:09 +0530

    Update reverse_vowels.py

--- python/task/reverse_vowels.py
+++ python/task/reverse_vowels.py
@@ -1,3 +1,18 @@
 #Input a string s from the user, reverse only all the vowels in the string and return it.
 
 #The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both cases.
+class Solution(object):
+    def reverseVowels(self, s):
+        vowels = set(list("aeiouAEIOU"))
+        s = list(s)
+        ptr_1, ptr_2 = 0, len(s) - 1
+        while ptr_1 < ptr_2:
+            if s[ptr_1] in vowels and s[ptr_2] in vowels:
+                s[ptr_1], s[ptr_2] = s[ptr_2], s[ptr_1]
+                ptr_1 += 1
+                ptr_2 -= 1
+            if s[ptr_1] not in vowels:
+                ptr_1 += 1
+            if s[ptr_2] not in vowels:
+                ptr_2 -= 1
+        return ''.join(s)
