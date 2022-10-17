c2a9ae2818e0f46919e54c4fa80d5919b7ef651f
Ankit Kumar <111765525+Ankit-0009@users.noreply.github.com>
2022-10-17 20:40:35 +0530

    Update 1_fibonaaci.cpp

--- CPP/task/1_fibonaaci.cpp
+++ CPP/task/1_fibonaaci.cpp
@@ -1,1 +1,18 @@
-//write a recursive function to print the nth fibonacci number
+#include<iostream>
+using namespace std;
+int main() {
+	int i,f=0,s=1,n,c=0;
+	cin>>n;
+	for(i=0;i<=n;i++){
+		if(i==0||i==1){
+			continue;
+		}
+		else{
+		c=f+s;
+		f=s;
+		s=c;
+		}
+	}
+	cout<<c;
+	return 0;
+}
