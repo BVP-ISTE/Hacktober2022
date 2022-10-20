521a62091a499fce4964218265791abfe1bc839b
Pratik-05 <pratik.darange@mitaoe.ac.in>
2022-10-20 19:11:32 +0530

    pattern triangle in cpp #278

--- CPP/error/patterntriangle.cpp
+++ CPP/error/patterntriangle.cpp
@@ -8,30 +8,17 @@ using namespace std;
 //* *
 //* * *
 //* * * *
-void patternrec(int n, int i)
-{
-    // base case
-    if (i == n)
-    {
-        return;
-    }
-
-    // recursice case
-    for (int j = 0; j < i; j++)
-    {
-        cout << "*"
-             << " ";
-    }
-    cout << endl;
-
-    patternrec(n, i);
-}
 
 int main()
 {
     int n;
     cin >> n;
 
-    patternrec(n, 0);
-    return 0;
+    for(int i = 1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    
 }
