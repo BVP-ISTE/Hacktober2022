7d98e1e3717155002af964a20e8bdbfc12ac351c
tanmaysharma17 <73175318+tanmaysharma17@users.noreply.github.com>
2022-10-22 22:41:13 +0530

    Update pascals_triangle.py
    Done! pls merge it

--- python/task/pascals_triangle.py
+++ python/task/pascals_triangle.py
@@ -6,3 +6,41 @@
 #  1 2 1
 # 1 3 3 1
 #Take number of rows as input from the user
+
+
+# Function to print
+# first n lines of
+# Pascal's Triangle
+def printPascal(n) :
+	
+	# Iterate through every line
+	# and print entries in it
+	for line in range(0, n) :
+		
+		# Every line has number of
+		# integers equal to line
+		# number
+		for i in range(0, line + 1) :
+			print(binomialCoeff(line, i),
+				" ", end = "")
+		print()
+	
+
+
+def binomialCoeff(n, k) :
+	res = 1
+	if (k > n - k) :
+		k = n - k
+	for i in range(0 , k) :
+		res = res * (n - i)
+		res = res // (i + 1)
+	
+	return res
+
+# Driver program
+n = 7
+printPascal(n)
+
+
+
+
