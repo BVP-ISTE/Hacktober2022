d41d17c9d4b7f66d76bf2213fd1dfcc42a2bd272
Souvik Dey <souvikpostjob01@gmail.com>
2022-10-22 07:22:42 +0000

    Hacktoberfest 2022 Solving #125

--- python/task/count_char.py
+++ python/task/count_char.py
@@ -6,6 +6,11 @@
 
 # Count occurrence of a character in a Python string
 
-sentence = 'The Udacity Udacity'
-ch = 'e'
+# sentence = 'The Udacity Udacity'
+# ch = 'e'
 
+sentence = input("Enter a valid sentence : ")
+ch = input("Enter a character to search occurance of in the above sentence : ")
+
+ch_occurance = sentence.count(ch)
+print("The world %s appears %s times in the sentence %s" %(ch, ch_occurance, sentence))
