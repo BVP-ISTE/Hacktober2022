1e6f7fdc4f3d562a127be5d62dd571af85c2bc3f
shivankdadhwal <56229707+shivankdadhwal@users.noreply.github.com>
2022-10-23 20:52:36 +0530

    Update add_binary.py
    Solved please merge

--- python/task/add_binary.py
+++ python/task/add_binary.py
@@ -7,3 +7,30 @@
 # Example 2:
 # Input: a = "1010", b = "1011"
 # Output: "10101"
+
+def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        res = []
+
+        i, j = len(a) - 1, len(b) - 1;
+        carry = 0
+
+        while i >= 0 or j >= 0:
+            total = carry
+            if i >= 0: 
+                total += int(a[i])
+                i -= 1
+            if j >= 0:
+                total += int(b[j])
+                j -= 1
+            res.append(`total % 2`)
+            carry = total / 2
+
+        if carry:
+            res.append(`carry`)
+
+        return "".join(res)[::-1]
