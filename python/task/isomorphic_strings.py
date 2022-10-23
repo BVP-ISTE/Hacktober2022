690b6c941c3bd14b099561bef57136aa6e99756f
shivankdadhwal <56229707+shivankdadhwal@users.noreply.github.com>
2022-10-23 20:44:40 +0530

    Update isomorphic_strings.py
    Done please merge it

--- python/task/isomorphic_strings.py
+++ python/task/isomorphic_strings.py
@@ -4,3 +4,6 @@
 
 #All occurrences of a character must be replaced with another character while preserving the order of characters. 
 #No two characters may map to the same character, but a character may map to itself.
+
+def isIsomorphic(self, s, t):
+        return len(set(zip(s, t))) == len(set(s)) and len(set(zip(t, s))) == len(set(t))
