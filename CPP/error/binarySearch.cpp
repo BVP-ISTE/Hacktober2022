daebd6136763a56d7af6ebc0aa327a540ad88ee9
sabhisharma-ise <2021is_sabhisharma_b@nie.ac.in>
2022-10-24 14:28:37 +0530

    Resolved error in binarysearch.cpp

--- CPP/error/binarySearch.cpp
+++ CPP/error/binarySearch.cpp
@@ -5,12 +5,13 @@ int binarySearch(int arr[],int size,int key){
     int start = 0;
     int end = size-1;
    
-    int mid = start + (end-start)/2;
     while (start<=end)
     {
+       int mid = start + (end-start)/2;
+       
        if (arr[mid] == key)
        {
-        return 0;
+        return mid;
        }
        if (key>arr[mid])
        {
@@ -20,8 +21,6 @@ int binarySearch(int arr[],int size,int key){
        {
         end = mid-1;
        }
-   
-     mid = start + (end-start)/2;
     }
     return -1;
 }
@@ -31,7 +30,7 @@ int main(){
 int even[6] = {2,4,6,8,12,18};
 int odd[6] = {3,8,11,14,16};
 int index = binarySearch(even,6,8);
-cout<<"index of 12 is : "<<index<<endl;
+cout<<"index of 8 is : "<<index<<endl;
 int oddindex = binarySearch(odd,5,11);
-cout<<"index of 3 is : "<<oddindex<<endl;
+cout<<"index of 11 is : "<<oddindex<<endl;
 }
