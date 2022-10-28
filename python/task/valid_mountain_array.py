15c163d826c48e8edbf9277df585d728a1696fad
Piyush Gupta <77025176+Piyush-Guptaa@users.noreply.github.com>
2022-10-28 17:01:59 +0530

    Update valid_mountain_array.py

--- python/task/valid_mountain_array.py
+++ python/task/valid_mountain_array.py
@@ -6,3 +6,18 @@
 #There exists some i with 0 < i < arr.length - 1 such that:
 #arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
 #arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+
+#My solution:
+
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        if len(arr) < 3:
+            return False
+        i = 0
+        while i < len(arr) - 1 and arr[i] < arr[i + 1]:
+            i += 1
+        if i == 0 or i == len(arr) - 1:
+            return False
+        while i < len(arr) - 1 and arr[i] > arr[i + 1]:
+            i += 1
+        return i == len(arr) - 1
