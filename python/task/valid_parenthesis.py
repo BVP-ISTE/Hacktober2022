16e05b4d7848e5149f994d324c79850063e28ddb
Piyush Gupta <77025176+Piyush-Guptaa@users.noreply.github.com>
2022-10-28 17:07:00 +0530

    Update valid_parenthesis.py

--- python/task/valid_parenthesis.py
+++ python/task/valid_parenthesis.py
@@ -5,3 +5,21 @@
 #Open brackets must be closed by the same type of brackets.
 #Open brackets must be closed in the correct order.
 #Every close bracket has a corresponding open bracket of the same type.
+
+#My solution:
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for i in s:
+            if i in ['(', '{', '[']:
+                stack.append(i)
+            elif i == ')' and stack and stack[-1] == '(':
+                stack.pop()
+            elif i == '}' and stack and stack[-1] == '{':
+                stack.pop()
+            elif i == ']' and stack and stack[-1] == '[':
+                stack.pop()
+            else:
+                return False
+        return not stack
